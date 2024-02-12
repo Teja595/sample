292bef0c1e4c8c2d341b432a943ca97f84796349
Teja <h20220119@pilani.bits-pilani.ac.in>
2024-02-12 17:19:07 +0530

    GET request to get users data from url in the front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  data: any; // This will store the fetched data
+  loading: boolean = false;
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+  
+  getData(): void {
+    this.loading = true;
+    const url = 'https://jsonplaceholder.typicode.com/users'; // Replace with your target URL
+    this.http.get(url)
+      .subscribe({
+        next: (response) => {
+          this.data = response; // Store the data received from the API
+          console.log(this.data); // Optional: Log the data to the console
+          this.loading = false;
+        },
+        error: (error) => {
+          console.error('There was an error!', error);
+          this.loading = false;
+        }
+      });
+    }
+
+
+
 }
