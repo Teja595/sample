ef881ef284805d669a6225e0e6c8d0b2287693ea
MUNNANGI TEJA VENKATA <teja.intern@srifincredit.com>
2024-02-13 11:15:35 +0530

    minor fixes

--- backend/backend/src/main/java/com/example/backend/User_s.java
+++ backend/backend/src/main/java/com/example/backend/User_s.java
@@ -2,6 +2,8 @@ package com.example.backend;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.util.Objects;
 
@@ -9,6 +11,7 @@ import java.util.Objects;
 @Entity
 public class User_s {
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String name;
   private String username;
